b81e6c21bcacd710e5e807cdc51be1fb5eac2584
kskadam7 <kadamkamalkishor8@gmail.com>
2023-08-30 18:48:03 +0530

    project

---
+++ Sparsh_Store/fronted/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Slider from "./Slider";
+
+const Home = () => {
+
+  return (
+    <>
+      <Navbar />
+      <Slider/>
+    
+      {/* <Products /> */}
+      {/* <Newsletter /> */}
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
---
+++ Sparsh_Store/fronted/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/808510/pexels-photo-808510.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Welcome",
+      desc: "Enjoy The Shopping!!!",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/793012/pexels-photo-793012.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Welcome",
+      desc: "Enjoy The Shopping!!!",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/4503735/pexels-photo-4503735.jpeg?auto=compress&cs=tinysrgb&w=600",
+      title: "Welcome",
+      desc: "Enjoy The Shopping!!!",
+      bg: "fbf0f4",
+    },
+  ];
